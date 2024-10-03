1aee4e04bd0e625104c8092bdca65a8c8cc6a0fc
MOISES GUZMAN MORA <moisomor22@gmail.com>
2024-10-02 21:11:23 -0600

    suu

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace hilosbasico3
@@ -10,6 +11,29 @@ namespace hilosbasico3
     {
         static void Main(string[] args)
         {
+            Thread hilo1 = new Thread(FuncionHilo);
+            Thread hilo2 = new Thread(FuncionHilo);
+            hilo1.Start();
+            hilo2.Start();
+            Console.ReadLine();
+
         }
+        public static void FuncionHilo()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hilo principal "+i);
+                Thread.Sleep(500);
+            }
+        }
+        public static void FuncionHilo2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hilo secundario " + i);
+                Thread.Sleep(500);
+            }
+        }
+
     }
 }
